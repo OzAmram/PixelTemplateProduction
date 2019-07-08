d40ad4ed3e42df06cd8776d0c89236054d4064c7
Oz Amram <oz.amram@gmail.com>
2019-07-08 11:30:45 -0400

    Fixing indentation

--- src/gen_xy_template2d_2t.cc
+++ src/gen_xy_template2d_2t.cc
@@ -27,12 +27,12 @@
 #include "template_utils.h"
 
 void gen_xy_template2d(const int nevents = 32000, const int npt = 300, const int nprm = 5, 
-		       const int maxarg = 5){
+        const int maxarg = 5){
 
     //size of the x and y arrays in template
     const int Nx = 21; // nbins in x
     const int Ny = 13; // nbins in y
-    
+
     double xpar[nprm][4], xytemp[Nx][Ny][7][7];
     int nxytry[7][7];
 
@@ -55,10 +55,10 @@ void gen_xy_template2d(const int nevents = 32000, const int npt = 300, const int
 
     int file_start, num_files, non_linear;
     float q100, q101, q100_frac, noise, common_frac, gain_frac, readout_noise;
-	fscanf(f_config,"%d %d %f %f %f %f %f %f %f %d", &file_start, &num_files, &noise, &q100, &q101, &q100_frac, &common_frac, 
+    fscanf(f_config,"%d %d %f %f %f %f %f %f %f %d", &file_start, &num_files, &noise, &q100, &q101, &q100_frac, &common_frac, 
             &gain_frac, &readout_noise, &non_linear);
-	fclose(f_config);
-	printf("processing %d files starting from %d, noise = %f, threshold0 = %f, threshold1 = %f," 
+    fclose(f_config);
+    printf("processing %d files starting from %d, noise = %f, threshold0 = %f, threshold1 = %f," 
             "rms threshold frac = %f, common_frac = %f, gain fraction = %f, readout noise = %f, nonlinear_resp = %d \n", 
             num_files, file_start, noise, q100, q101, q100_frac, common_frac, gain_frac, readout_noise, non_linear);
 
@@ -99,7 +99,7 @@ void gen_xy_template2d(const int nevents = 32000, const int npt = 300, const int
 
         int nelec;
 
-	zero_3d_array(pixev, Nx, Ny, nevents);
+        zero_3d_array(pixev, Nx, Ny, nevents);
 
         //loop over events 
         int n;
@@ -124,8 +124,8 @@ void gen_xy_template2d(const int nevents = 32000, const int npt = 300, const int
             //note the confusing index switch. input arrays are 13x21, we swap
             //to 21x13
             for(int j=0; j<Ny; j++){ // 13
-	        for (int i=0; i<Nx; i++){ // 21
-		  fscanf(f_evts, " %f ", &bixin[i][j]); // this is charge in 1000s of e-
+                for (int i=0; i<Nx; i++){ // 21
+                    fscanf(f_evts, " %f ", &bixin[i][j]); // this is charge in 1000s of e-
                 }
             }
 
@@ -137,16 +137,16 @@ void gen_xy_template2d(const int nevents = 32000, const int npt = 300, const int
 
             //hit position bins (7 bins within a pixel)
             //last bin is the same as first, translated 1 pixel over
-	    // i.e. bin0 px0 = bin7 px-1 
-	    // and  bin7 px0 = bin0 px1
+            // i.e. bin0 px0 = bin7 px-1 
+            // and  bin7 px0 = bin0 px1
             //divide into 2d bins (6x6) for max charge
 
-	    // restrict from 0 to 6;    
+            // restrict from 0 to 6;    
             int i2dn  = int(xhit/xsize*6 + 3.5);
             i2dn = std::min(6, std::max(i2dn, 0));
             i2d[n] = i2dn;
 
-	    //restrict from 0 to 6; 
+            //restrict from 0 to 6; 
             int j2dn  = int(yhit/ysize*6 + 3.5);
             j2dn = std::min(6, std::max(j2dn, 0));
             j2d[n] = j2dn;
@@ -157,8 +157,8 @@ void gen_xy_template2d(const int nevents = 32000, const int npt = 300, const int
                 for(int j=0; j<Ny; j++){
                     double qin = bixin[i][j] * rten;
                     if(qin < 0.) qin = 0.;
-		    pixev[i][j][n] = qin;
-		    qsum[n] += qin;
+                    pixev[i][j][n] = qin;
+                    qsum[n] += qin;
                 }
             }
             qavg += qsum[n];
@@ -166,7 +166,7 @@ void gen_xy_template2d(const int nevents = 32000, const int npt = 300, const int
         } // finish loop over events
 
         int nmc = n; // total number of events
- 
+
         printf("finish reading run %i read %i events, qsum = %.1f \n", iFile, nmc, qavg);
 
         fclose(f_evts);
@@ -185,107 +185,107 @@ void gen_xy_template2d(const int nevents = 32000, const int npt = 300, const int
             if(qsum[n] < qavg){
 
                 //fill 2d 7x7 2d template for average charge determination
-	        int k = i2d[n]; // k is pixel?
-	        int l = j2d[n]; // l is pixel?
+                int k = i2d[n]; // k is pixel?
+                int l = j2d[n]; // l is pixel?
                 nxytry[k][l] +=1;
 
-		for(int i=0; i < Nx; i++){
-		  for(int j=0; j < Ny; j++){
-		    xytemp[i][j][k][l] += pixev[i][j][n];
-		  }
-		}
+                for(int i=0; i < Nx; i++){
+                    for(int j=0; j < Ny; j++){
+                        xytemp[i][j][k][l] += pixev[i][j][n];
+                    }
+                }
 
                 for(int i=0; i < Nx; i++){
-		  for(int j=0; j < Ny; j++){
-		    // 0th bin of new pixel same as last bin of previous pixel   
-		    if(k==0 && i>0){ // bin0px(i) = bin6px(i-1) 
-		      xytemp[i][j][6][l] += pixev[i-1][j][n]; // adding to bin6 of that same pix 
-		    }
-		    else if(k==6 && i>0){ // bin6px(i) = bin0px(i+1) 
-		      xytemp[i-1][j][0][l] += pixev[i][j][n];  // adding to bin0 of pix-1 
-		    }
+                    for(int j=0; j < Ny; j++){
+                        // 0th bin of new pixel same as last bin of previous pixel   
+                        if(k==0 && i>0){ // bin0px(i) = bin6px(i-1) 
+                            xytemp[i][j][6][l] += pixev[i-1][j][n]; // adding to bin6 of that same pix 
+                        }
+                        else if(k==6 && i>0){ // bin6px(i) = bin0px(i+1) 
+                            xytemp[i-1][j][0][l] += pixev[i][j][n];  // adding to bin0 of pix-1 
+                        }
 
-		    if(l==0 && j>0){
-		      xytemp[i][j][k][6] += pixev[i][j-1][n]; // adding to bin6 of that same pix 
-		    }
-		    else if(l==6 && j>0){
-		      xytemp[i][j-1][k][0] += pixev[i][j][n]; // adding to bin0 of pix-1
-		    }
+                        if(l==0 && j>0){
+                            xytemp[i][j][k][6] += pixev[i][j-1][n]; // adding to bin6 of that same pix 
+                        }
+                        else if(l==6 && j>0){
+                            xytemp[i][j-1][k][0] += pixev[i][j][n]; // adding to bin0 of pix-1
+                        }
 
-		    if(i>0 && j>0) {
-                      if(k==0 && l==0){
-                        xytemp[i][j][6][6] += pixev[i-1][j-1][n];
-                      }
-                      else if(k==6 && l==6){
-                        xytemp[i-1][j-1][0][0] += pixev[i][j][n];
-                      }
-                      else if(k==0 && l==6){
-                        xytemp[i][j-1][6][0] += pixev[i-1][j][n];
-                      }
-                      else if(k==6 && l==0){
-                        xytemp[i-1][j][0][6] += pixev[i][j-1][n];
-                      }
-                    }
+                        if(i>0 && j>0) {
+                            if(k==0 && l==0){
+                                xytemp[i][j][6][6] += pixev[i-1][j-1][n];
+                            }
+                            else if(k==6 && l==6){
+                                xytemp[i-1][j-1][0][0] += pixev[i][j][n];
+                            }
+                            else if(k==0 && l==6){
+                                xytemp[i][j-1][6][0] += pixev[i-1][j][n];
+                            }
+                            else if(k==6 && l==0){
+                                xytemp[i-1][j][0][6] += pixev[i][j-1][n];
+                            }
+                        }
 
-		  } // end Ny
+                    } // end Ny
                 } // end Nx
-	    
-		if(k < 0 || k > 6){
-		  printf("Problem with event %i, index (k) is =%i \n", n, k);
+
+                if(k < 0 || k > 6){
+                    printf("Problem with event %i, index (k) is =%i \n", n, k);
                 }
                 else if(k==0) nxytry[6][l] += 1;
-		else if(k==6) nxytry[0][l] += 1;
+                else if(k==6) nxytry[0][l] += 1;
 
                 if(l < 0 || l > 6){
                     printf("Problem with event %i, index (l) is =%i \n", n, l);
                 }
                 else if(l==0) nxytry[k][6] += 1;
-		else if(l==6) nxytry[k][0] += 1;
+                else if(l==6) nxytry[k][0] += 1;
 
-		if(k==0 && l==0){
-		  nxytry[6][6] += 1;
-		}
-		else if(k==6 && l==6){
-		  nxytry[0][0] += 1;
-		}
-		else if(k==0 && l==6){
-		  nxytry[6][0] += 1;
-		}
-		else if(k==6 && l==0){
-		  nxytry[0][6] += 1;
-		}
+                if(k==0 && l==0){
+                    nxytry[6][6] += 1;
+                }
+                else if(k==6 && l==6){
+                    nxytry[0][0] += 1;
+                }
+                else if(k==0 && l==6){
+                    nxytry[6][0] += 1;
+                }
+                else if(k==6 && l==0){
+                    nxytry[0][6] += 1;
+                }
             } 
         }//end loop over events
 
-	//renorm the distributions, find the maximum average pixel signal in 7x7 grid
+        //renorm the distributions, find the maximum average pixel signal in 7x7 grid
         float pixmax = 0.;
-	int imin = Nx;
-	int imax = 0;
-	int jmin = Ny;
-	int jmax = 0;
-	for(int k =0; k<7; k++){
-	  for(int l =0; l<7; l++){
-	    for(int i =0; i<Nx; i++){
-	      for(int j =0; j<Ny; j++){
-		float sigxy = xytemp[i][j][k][l] / float(nxytry[k][l]);
-		xytemp[i][j][k][l] = sigxy;
-		if(sigxy > pixmax) pixmax = sigxy;
-		if(sigxy > thr10) {
-		  if(i < imin) imin=i+1; // this is to match the fortran output - is this even used later?
-		  if(i > imax) imax=i+1;
-		  if(j < jmin) jmin=j+1;
-		  if(j > jmax) jmax=j+1;
-		}
-	      }
-	    }
-	  }
+        int imin = Nx;
+        int imax = 0;
+        int jmin = Ny;
+        int jmax = 0;
+        for(int k =0; k<7; k++){
+            for(int l =0; l<7; l++){
+                for(int i =0; i<Nx; i++){
+                    for(int j =0; j<Ny; j++){
+                        float sigxy = xytemp[i][j][k][l] / float(nxytry[k][l]);
+                        xytemp[i][j][k][l] = sigxy;
+                        if(sigxy > pixmax) pixmax = sigxy;
+                        if(sigxy > thr10) {
+                            if(i < imin) imin=i+1; // this is to match the fortran output - is this even used later?
+                            if(i > imax) imax=i+1;
+                            if(j < jmin) jmin=j+1;
+                            if(j > jmax) jmax=j+1;
+                        }
+                    }
+                }
+            }
         }
 
-	// set parameters to 0
-	for(int p=0; p<nprm; p++){
-	  for(int i=0; i<4; i++){
-	    xpar[p][i] = 0;
-	  }
+        // set parameters to 0
+        for(int p=0; p<nprm; p++){
+            for(int i=0; i<4; i++){
+                xpar[p][i] = 0;
+            }
         }
 
         //open summary files
@@ -295,28 +295,28 @@ void gen_xy_template2d(const int nevents = 32000, const int npt = 300, const int
         sprintf(file_out, "zptemp_%i.txt", iFile);
         FILE *zptemp_file = fopen(file_out, "w+");
         fprintf(zptemp_file, "%9.6f %9.6f %9.6f \n", cosx, cosy, cosz);
-	fprintf(zptemp_file, "%8.1f %8.1f %2i %2i %2i %2i\n", qavg,pixmax,imin,imax,jmin,jmax); 
-	for(int k=2; k <4; k++){
-	  for(int p=0; p<nprm; p++){
-	    fprintf(zptemp_file, "%15.8E ", xpar[p][k]); // these should be 0 now
-	  }
-	  fprintf(zptemp_file, "\n");
-	}
-	
-	for(int l=0; l <7; l++){
-	  for(int k=0; k <7; k++){
-            //float ycenter = ((l+1)*0.166667 - 0.166667) *ysize; I think this is bugged in the original one(?)
-            float ycenter = ((l+1)*0.125 - 0.625) *ysize;
-	    float xcenter = ((k+1)*0.166667 - 0.666667) *xsize;
-            fprintf(zptemp_file, "biny %2i,  ycenter = %8.2f um, binx %2i, xcenter = %8.2f um \n", l+1, ycenter, k+1, xcenter);
-	    for(int j=0; j<Ny; j++){
-	      for(int i=0; i<Nx; i++){
-		fprintf(zptemp_file, "%8.1f ", xytemp[i][j][k][l] );
-	      }
-	      fprintf(zptemp_file, "\n"); 
+        fprintf(zptemp_file, "%8.1f %8.1f %2i %2i %2i %2i\n", qavg,pixmax,imin,imax,jmin,jmax); 
+        for(int k=2; k <4; k++){
+            for(int p=0; p<nprm; p++){
+                fprintf(zptemp_file, "%15.8E ", xpar[p][k]); // these should be 0 now
             }
-	  }
-	}
+            fprintf(zptemp_file, "\n");
+        }
+
+        for(int l=0; l <7; l++){
+            for(int k=0; k <7; k++){
+                //float ycenter = ((l+1)*0.166667 - 0.166667) *ysize; I think this is bugged in the original one(?)
+                float ycenter = ((l+1)*0.125 - 0.625) *ysize;
+                float xcenter = ((k+1)*0.166667 - 0.666667) *xsize;
+                fprintf(zptemp_file, "biny %2i,  ycenter = %8.2f um, binx %2i, xcenter = %8.2f um \n", l+1, ycenter, k+1, xcenter);
+                for(int j=0; j<Ny; j++){
+                    for(int i=0; i<Nx; i++){
+                        fprintf(zptemp_file, "%8.1f ", xytemp[i][j][k][l] );
+                    }
+                    fprintf(zptemp_file, "\n"); 
+                }
+            }
+        }
 
         fclose(zptemp_file);
 
